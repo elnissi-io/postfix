c7991c44a7b3bf6cc8875ec0b76d336ed2f8b2d2
dennsgh <opschon@gmail.com>
2024-04-26 19:42:20 +0200

    change ownership of keys to opendkim

--- entrypoint.sh
+++ entrypoint.sh
@@ -109,6 +109,7 @@ setup_opendkim() {
   else
     echo "OpenDKIM key directory for ${PRIMARY_DOMAIN} not found. Ensure the keys are in place or update OPENDKIM_KEYS_DIR if necessary."
     echo "Make sure ${OPENDKIM_KEYS_DIR}/${PRIMARY_DOMAIN}/${OPENDKIM_DEFAULT_SELECTOR}.private exists."
+    chown -R opendkim:opendkim ${OPENDKIM_KEYS_DIR}
   fi
 }
 
